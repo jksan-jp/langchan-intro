96050ebb17f32ed33e5fc1e6c5214fdf31a6e0a8
jksan <jksan.jp@gmail.com>
2024-01-13 23:02:32 +0900

    openai追加

--- hello.py
+++ hello.py
@@ -1,2 +1,22 @@
-import sys
-print(f"Python version {sys.version}")
+import os
+import json
+from openai import OpenAI
+
+MODEL = "gpt-3.5-turbo"
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model=MODEL,
+)
+
+print(json.dump(chat_completion, indent=2, ensure_ascii=False))
